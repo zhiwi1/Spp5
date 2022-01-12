1e26d8bec442c386c1e929760bd9202fa985abae
Ivan_Zhyuliuk <zhyuliuk.ivan@gmail.com>
2022-01-12 13:20:00 +0300

    edit provider

---
+++ DependencyInjectionContainer/Provider/SingletonContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainer.Provider
+{
+    internal class SingletonContainer
+    {
+    }
+}
